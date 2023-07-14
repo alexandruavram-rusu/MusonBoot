f5cdcff2bbdd9bafde7464e57fe01b33a52c96ce
Alexandru Avram-Rusu <83804634+alexandruavram-rusu@users.noreply.github.com>
2023-07-14 12:43:01 +0300

    muson

--- MusonBoot.py
+++ MusonBoot.py
@@ -1,31 +1,44 @@
-from telegram.ext import Updater, MessageHandler, Filters
-from telegram import InputMediaPhoto
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from triggers import triggers  # Import the triggers from the separate file
 
+TOKEN = '6246555581:AAGB4Fb8B1n9656SbBO8PZ8a41smVbNbqN8'
+BOT_USERNAME = '@MusonulBot'
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-def message_handler(update, context):
-    text = update.message.text.lower()
-    chat_id = update.message.chat_id
+# Create a bot instance
+bot = Bot(token=TOKEN)
+
+# Create a dispatcher instance
+storage = MemoryStorage()
+dispatcher = Dispatcher(bot, storage=storage)
+
+@dispatcher.message_handler(content_types=types.ContentTypes.TEXT)
+async def message_handler(message: types.Message):
+    text = message.text.lower()
+    chat_id = message.chat.id
 
     for trigger, data in triggers.items():
         if isinstance(trigger, list):
-            if any(t in text for t in trigger):
+            if all(t in text for t in trigger):
                 image_path = data['image_path']
                 caption = data['caption']
-                context.bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'), caption=caption)
+                with open(image_path, 'rb') as photo:
+                    await bot.send_photo(chat_id=chat_id, photo=photo, caption=caption)
                 break
 
-        
-# Create an Updater object with your bot's API token
-updater = Updater(token='6246555581:AAGB4Fb8B1n9656SbBO8PZ8a41smVbNbqN8', use_context=True)
+async def startup(dp):
+    await bot.send_message(chat_id='@channel', text='Bot started!')
 
-# Get the dispatcher to register handlers
-dispatcher = updater.dispatcher
-
-# Register the message handler function
-message_handler = MessageHandler(Filters.text, message_handler)
-dispatcher.add_handler(message_handler)
+async def shutdown(dp):
+    await bot.send_message(chat_id='@channel', text='Bot stopped!')
+    await dp.storage.close()
+    await dp.storage.wait_closed()
 
 # Start the bot
-updater.start_polling()
+if __name__ == '__main__':
+    from aiogram import executor
+    executor.start_polling(dispatcher, on_startup=startup, on_shutdown=shutdown)
